014de4a787d7067daba78e1b88a36c87d79563e0
Florian Cassayre <florian.cassayre@gmail.com>
2020-03-22 11:07:34 +0100

    Updated visual.

--- covid2.py
+++ covid2.py
@@ -24,6 +24,25 @@ stats_per_country = df.groupby('country')[['cases', 'deaths']].sum()
 # Filtering countries
 countries_of_interest = stats_per_country[(stats_per_country.cases > cases_treshold) & (~stats_per_country.index.str.contains('Cases'))].cases.sort_values().index.tolist()
 
+def cumsum(country):
+    data = df[df.country == country].copy().set_index('date')[['cases', 'deaths']].cumsum()
+    data = data[data.cases >= start_at]  # Normalize starting point
+    data = data.reset_index()
+
+    return data
+
+per_country = {}
+
+fits = []
+
+max_period = 0
+for country in countries_of_interest:
+    data = cumsum(country)
+    per_country[country] = data
+    if country != 'China':
+        max_period = max(len(data) + 3, max_period)  # Add a few days
+
+
 matplotlib.rcParams['figure.dpi'] = 300
 
 fig, ax = plt.subplots(figsize=(10, 5))
@@ -34,18 +53,40 @@ plt.grid(True)
 
 texts = []
 
+def get_color(country):
+    if country == 'China':
+        return "#cf6102"
+    elif country == 'South Korea' or country == 'Japan' or country == 'Singapore':
+        return "#02cfcf"
+    elif country == 'Italy':
+        return "#018c16"
+    elif country == 'France':
+        return "#437fba"
+    elif country == 'United States of America':
+        return "#0d30db"
+    elif country == "Spain":
+        return "#dbb90d"
+    elif country == "Germany":
+        return "#423d24"
+    elif country == "United Kingdom":
+        return "#9c1e74"
+    elif country == "Iran":
+        return "#96a61b"
+    else:
+        return None
+
 for country in countries_of_interest:
     country_display = country.replace("_", " ")
 
-    data = df[df.country == country].copy().set_index('date')[['cases', 'deaths']].cumsum()
-    data = data[data.cases >= start_at]  # Normalize starting point
-    data = data.reset_index()
+    data = per_country[country]
 
-    to_plot = data.cases
+    to_plot = data.head(max_period).cases
 
     highlight = len(data) > 10
 
-    color = None if highlight else 'lightgray'
+    color = get_color(country_display) if highlight else 'lightgray'
+    font_weight = 'bold' if color is not None else None
+    color = "#919191" if color is None else color
     line_width = 1 if highlight else 0.5
     marker_kind = 'o' if highlight else None
     marker_size = 2
@@ -60,8 +101,9 @@ for country in countries_of_interest:
     if highlight:
         plt.plot(to_plot.tail(1), marker=marker_kind, markersize=marker_size_last, color=color, markeredgewidth=marker_edge_last, markeredgecolor='black')
         last_pos = last.reset_index().to_numpy()[0]
-        text = ax.annotate(country_display, (last_pos[0] + 0.3, last_pos[1]),
-                           size=8, color=color,
+        display = "%s (after %s days)" % (country_display, max_period - 1) if country == 'China' else country_display
+        text = ax.annotate(display, (last_pos[0] + 0.3, last_pos[1]),
+                           size=8, color=color, weight=font_weight,
                            path_effects=[matplotlib.patheffects.withStroke(linewidth=1, foreground="white")])
         texts.append(text)
 
@@ -84,13 +126,13 @@ while loop:
             break
     i += 1
 
-ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: '{:,}'.format(x).replace(",", " ")))
+ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: '{:,}'.format(x)))
 plt.yticks(ticks)
 
 adjust_text(texts)
 
-plt.xlabel("Nombre de jours depuis le %sème cas" % start_at)
+plt.xlabel("Number of days since the %sth cases" % start_at)
 
-plt.title("Nombre de cas cumulés jour par jour depuis le %sème cas" % start_at)
+plt.title("Cumulative number of cases per country on a daily basis since %sth case" % start_at)
 
 plt.show()
